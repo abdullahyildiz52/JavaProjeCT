2da35003f840c09d5e9f120516c3259c084348a2
abdullahyildiz52 <ce_abdullahyildiz@hotmail.com>
2023-05-05 23:25:29 +0300

    day16 eksikler tamamlandi

---
+++ src/day16_ArrayList/C09_ForEachLoop.java
@@ -0,0 +1,35 @@
+package day16_ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class C09_ForEachLoop {
+    public static void main(String[] args) {
+
+        //Soru 3- Verilen String bir array’deki her bir elementi kontrol edip,
+        //  - Kelimenin uzunlugu cift sayi ise ilk yarisini
+        //  - Kelimenin uzunlugu tek sayi ise ortadaki harf dahil ikinci yarisini
+        //  yeni bir listeye ekleyip yazdirin.
+
+        String[] arr = {"arabam","mirkelam","mehmet","hasan","kitaplik"};
+        isimManipilasyonMethodu(arr);
+    }
+    public static void isimManipilasyonMethodu(String[] arr){
+
+        // Manipule edilen kelimeleri eklemek icin
+        List<String> manipuleKelimeler = new ArrayList<>();
+
+        // arr icindeki butun kelimelere tek tek ulasmak
+        for (String each: arr
+        ) {
+            // mehmet // kelimenin uzunlugu cift sayi ise
+            if (each.length() %2 ==0){
+                // ilk yarisi icin index sifir'dan uzunlugun yarisi nakar
+                manipuleKelimeler.add(each.substring(0,each.length()/2));
+            }else {
+                manipuleKelimeler.add(each.substring((each.length()-1)/2));
+            }
+        }
+        System.out.println(manipuleKelimeler);
+    }
+}
---
+++ src/day16_ArrayList/C13_ListeOlusturmaVeElemanEkleme.java
@@ -0,0 +1,19 @@
+package day16_ArrayList;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class C13_ListeOlusturmaVeElemanEkleme {
+    public static void main(String[] args) {
+        // Liste+ elemanlarin turu + isim = new LinkedList<>();
+        List<String> isimListesi = new LinkedList<>();
+        // add ile ekleniyor
+        isimListesi.add("Hasan");
+        isimListesi.add("Mehmet");
+        isimListesi.add("Veli");
+        isimListesi.add("Ars");
+        isimListesi.add("Murat");
+        isimListesi.add("Mustafa");
+        System.out.println(isimListesi);
+    }
+}
